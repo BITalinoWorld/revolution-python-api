66832854d24bacaf756d772b0158eeb0ca696cb4
Takuma Hashimoto <ma16079@shibaura-it.ac.jp>
2018-01-24 13:55:59 +0900

    modify exception

--- bitalino.py
+++ bitalino.py
@@ -28,7 +28,7 @@ def find():
     if platform.system() == 'Windows' or platform.system() == 'Linux':
         try:
             import bluetooth
-        except Exception, e:
+        except Exception as e:
             raise Exception(ExceptionCode.IMPORT_FAILED + str(e))
         nearby_devices = bluetooth.discover_devices(lookup_names=True)
         return nearby_devices
@@ -84,7 +84,7 @@ class BITalino(object):
             if platform.system() == 'Windows' or platform.system() == 'Linux':
                 try:
                     import bluetooth
-                except Exception, e:
+                except Exception as e:
                     raise Exception(ExceptionCode.IMPORT_FAILED + str(e))
                 self.socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
                 self.socket.connect((macAddress, 1))
